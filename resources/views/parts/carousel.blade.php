09472961b47d5110cc09233ddbf6a192b8c38a1c
Azril Nazli Alias <azril.nazli@gmail.com>
2020-11-07 23:27:36 +0800

    path images

--- resources/views/parts/carousel.blade.php
+++ resources/views/parts/carousel.blade.php
@@ -3,7 +3,7 @@
     <div class="carousel-row text-center">
         @foreach($data as $video)
         <div class="carousel-tile">
-            <a href="{{ route('play',$video->id) }}"><img  style="width:200px;height:300px" src="http://auth.test/uploads/{{ $video->id }}/images/file-1-small.png" /></a>
+            <a href="{{ route('play',$video->id) }}"><img  style="width:200px;height:300px" src="{{ config('settings.asset_server') . $video->id }}/images/file-1-small.png" /></a>
         </div>
         @endforeach
     </div>
