c4cf18eb4f56390b71751defa8bd6204cc083513
Azril Nazli Alias <azril.nazli@gmail.com>
2020-11-08 10:37:09 +0800

    by_category view path

--- resources/views/by_category.blade.php
+++ resources/views/by_category.blade.php
@@ -10,21 +10,19 @@ $videosPerRow = 9 ;
 
 @endphp
 
-@if($countVideos>0)
-    <div class="carousel">
-        <div class="carousel-row text-center">
-        @for ($i = 0 ; $i < $countVideos; $i++)
+<div class="carousel">
+    <div class="carousel-row text-center">
+    @for ($i = 0 ; $i < $countVideos; $i++)
 
-            @if ($i % $videosPerRow == 0) 
-            <hr class="border-0"> 
-            @endif
+        @if ($i % $videosPerRow == 0) 
+        <hr class="border-0"> 
+        @endif
 
-            <div class="carousel-tile">
-                <a href="{{ route('play',$data[$i]->id) }}"><img  style="width:200px;height:300px" src="{{ config('settings.asset_server') . $data->id }}/images/file-1-small.png" /></a>
-            </div>
-        @endfor
-
+        <div class="carousel-tile">
+            <a href="{{ route('play',$data[$i]->id) }}"><img  style="width:200px;height:300px" src=" {{ config('settings.asset_server') .  $data[$i]->id }}/images/file-1-small.png" /></a>
         </div>
+    @endfor
+
     </div>
-@endif
+</div>
 @endsection 
