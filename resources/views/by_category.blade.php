d9966e3914d2ff5e03a3fe14608be0dde68a5ff1
Azril Nazli Alias <azril.nazli@gmail.com>
2020-11-08 10:34:32 +0800

    by_category view path

--- resources/views/by_category.blade.php
+++ resources/views/by_category.blade.php
@@ -20,7 +20,7 @@ $videosPerRow = 9 ;
             @endif
 
             <div class="carousel-tile">
-                <a href="{{ route('play',$data[$i]->id) }}"><img  style="width:200px;height:300px" src="{{ config('settings.asset_server') . $video->id }}/images/file-1-small.png" /></a>
+                <a href="{{ route('play',$data[$i]->id) }}"><img  style="width:200px;height:300px" src="{{ config('settings.asset_server') . $data->id }}/images/file-1-small.png" /></a>
             </div>
         @endfor
 
