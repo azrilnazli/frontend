67de81274e47e748c10209cd793ae48d588dfa86
Azril Nazli Alias <azril.nazli@gmail.com>
2020-12-22 11:07:44 +0800

    repair main meni for mobile view and desktop

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -25,11 +25,7 @@
                                  <li class="menu-item">
                                     <a href="show-category.html">Shows</a>
                                  </li>
-                                 {{--
-                                 <li class="menu-item">
-                                    <a href="movie-category.html">Movies</a>
-                                 </li>
-                                 --}}
+ 
                               </ul>
                            </div>
                         </div>
@@ -41,7 +37,7 @@
                            <div class="more-menu" aria-labelledby="dropdownMenuButton">
                               <div class="navbar-right position-relative">
                                  <ul class="d-flex align-items-center justify-content-end list-inline m-0">
-                                   {{--
+                                 
                                     <li>
                                        <a href="#" class="search-toggle">
                                        <i class="ri-search-line"></i>
@@ -57,60 +53,20 @@
                                        </div>
                                     </li>
 
-                                    --}}
-                                    <li class="nav-item nav-icon">
-                                       <a href="#" class="search-toggle position-relative">
-                                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22"
-                                             height="22" class="noti-svg">
-                                             <path fill="none" d="M0 0h24v24H0z" />
-                                             <path
-                                                d="M18 10a6 6 0 1 0-12 0v8h12v-8zm2 8.667l.4.533a.5.5 0 0 1-.4.8H4a.5.5 0 0 1-.4-.8l.4-.533V10a8 8 0 1 1 16 0v8.667zM9.5 21h5a2.5 2.5 0 1 1-5 0z" />
-                                          </svg>
-                                          <span class="bg-danger dots"></span>
-                                       </a>
-                                       <div class="iq-sub-dropdown">
-                                          <div class="iq-card shadow-none m-0">
-                                             <div class="iq-card-body">
-                                                <a href="#" class="iq-sub-card">
-                                                   <div class="media align-items-center">
-                                                      <img src="/images/notify/thumb-1.jpg" class="img-fluid mr-3"
-                                                         alt="streamit" />
-                                                      <div class="media-body">
-                                                         <h6 class="mb-0 ">Booppoy Bitty</h6>
-                                                         <small class="font-size-12"> just now</small>
-                                                      </div>
-                                                   </div>
-                                                </a>
-                                                <a href="#" class="iq-sub-card">
-                                                   <div class="media align-items-center">
-                                                      <img src="/images/notify/thumb-2.jpg" class="img-fluid mr-3"
-                                                         alt="streamit" />
-                                                      <div class="media-body">
-                                                         <h6 class="mb-0 ">The Last Breath</h6>
-                                                         <small class="font-size-12">15 minutes ago</small>
-                                                      </div>
-                                                   </div>
-                                                </a>
-                                                <a href="#" class="iq-sub-card">
-                                                   <div class="media align-items-center">
-                                                      <img src="/images/notify/thumb-3.jpg" class="img-fluid mr-3"
-                                                         alt="streamit" />
-                                                      <div class="media-body">
-                                                         <h6 class="mb-0 ">The Hero Camp</h6>
-                                                         <small class="font-size-12">1 hour ago</small>
-                                                      </div>
-                                                   </div>
-                                                </a>
-                                             </div>
-                                          </div>
-                                       </div>
-                                    </li>
-                                    
+                                 
+                                    {{-- for mobile menu --}}
                                     <li>
-                                       <a href="#" class="iq-user-dropdown search-toggle d-flex align-items-center">
-                                       <img src="/images/user/user.jpg" class="img-fluid avatar-40 rounded-circle"
-                                          alt="user">
-                                       </a>
+                                    <a href="#" class="iq-user-dropdown search-toggle p-0 d-flex align-items-center"
+                                    data-toggle="search-toggle">
+
+                                 
+
+                                 @if(file_exists( public_path().'/thumbnails/avatar-'. auth()->user()->id . '.png' ))
+                                    <img src="{{ '/thumbnails/avatar-'. auth()->user()->id . '.png' }}" class="img-fluid avatar-40 rounded-circle" alt="user">
+                                 @else 
+                                    <img src="/images/user/user.jpg" class="img-fluid avatar-40 rounded-circle" alt="user">
+                                 @endif
+                                 </a>
                                        <div class="iq-sub-dropdown iq-user-dropdown">
                                           <div class="iq-card shadow-none m-0">
                                              <div class="iq-card-body p-0 pl-3 pr-3">
@@ -134,26 +90,27 @@
                                                       </div>
                                                    </div>
                                                 </a>
-                                                <a href="pricing-plan.html" class="iq-sub-card setting-dropdown">
-                                                   <div class="media align-items-center">
-                                                      <div class="right-icon">
-                                                         <i class="ri-settings-4-line text-primary"></i>
-                                                      </div>
-                                                      <div class="media-body ml-3">
-                                                         <h6 class="mb-0 ">Pricing Plan</h6>
-                                                      </div>
-                                                   </div>
-                                                </a>
-                                                <a href="" class="iq-sub-card setting-dropdown">
+     
+
+                                                <a href="{{ route('logout') }}" 
+                                                   class="iq-sub-card setting-dropdown"
+                                                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                                   
                                                    <div class="media align-items-center">
                                                       <div class="right-icon">
                                                          <i class="ri-logout-circle-line text-primary"></i>
                                                       </div>
                                                       <div class="media-body ml-3">
-                                                         <h6 class="mb-0">Loccgout</h6>
+                                                         <h6 class="mb-0 ">Logout</h6>
+                                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                            @csrf
+                                                         </form>
                                                       </div>
                                                    </div>
                                                 </a>
+
+
+                                                
                                              </div>
                                           </div>
                                        </div>
@@ -178,53 +135,9 @@
                                     </form>
                                  </div>
                               </li>
-                              <li class="nav-item nav-icon">
-                                 <a href="#" class="search-toggle" data-toggle="search-toggle">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22"
-                                       class="noti-svg">
-                                       <path fill="none" d="M0 0h24v24H0z" />
-                                       <path
-                                          d="M18 10a6 6 0 1 0-12 0v8h12v-8zm2 8.667l.4.533a.5.5 0 0 1-.4.8H4a.5.5 0 0 1-.4-.8l.4-.533V10a8 8 0 1 1 16 0v8.667zM9.5 21h5a2.5 2.5 0 1 1-5 0z" />
-                                    </svg>
-                                    <span class="bg-danger dots"></span>
-                                 </a>
-                                 <div class="iq-sub-dropdown">
-                                    <div class="iq-card shadow-none m-0">
-                                       <div class="iq-card-body">
-                                          <a href="#" class="iq-sub-card">
-                                             <div class="media align-items-center">
-                                                <img src="/images/notify/thumb-1.jpg" class="img-fluid mr-3"
-                                                   alt="streamit" />
-                                                <div class="media-body">
-                                                   <h6 class="mb-0 ">Boot Bitty</h6>
-                                                   <small class="font-size-12"> just now</small>
-                                                </div>
-                                             </div>
-                                          </a>
-                                          <a href="#" class="iq-sub-card">
-                                             <div class="media align-items-center">
-                                                <img src="/images/notify/thumb-2.jpg" class="img-fluid mr-3"
-                                                   alt="streamit" />
-                                                <div class="media-body">
-                                                   <h6 class="mb-0 ">The Last Breath</h6>
-                                                   <small class="font-size-12">15 minutes ago</small>
-                                                </div>
-                                             </div>
-                                          </a>
-                                          <a href="#" class="iq-sub-card">
-                                             <div class="media align-items-center">
-                                                <img src="/images/notify/thumb-3.jpg" class="img-fluid mr-3"
-                                                   alt="streamit" />
-                                                <div class="media-body">
-                                                   <h6 class="mb-0 ">The Hero Camp</h6>
-                                                   <small class="font-size-12">1 hour ago</small>
-                                                </div>
-                                             </div>
-                                          </a>
-                                       </div>
-                                    </div>
-                                 </div>
-                              </li>
+
+
+                              {{-- for Desktop menu --}}
                               <li class="nav-item nav-icon">
                                  <a href="#" class="iq-user-dropdown search-toggle p-0 d-flex align-items-center"
                                     data-toggle="search-toggle">
@@ -237,6 +150,7 @@
                                     <img src="/images/user/user.jpg" class="img-fluid avatar-40 rounded-circle" alt="user">
                                  @endif
                                  </a>
+
                                  <div class="iq-sub-dropdown iq-user-dropdown">
                                     <div class="iq-card shadow-none m-0">
                                        <div class="iq-card-body p-0 pl-3 pr-3">
@@ -260,16 +174,7 @@
                                                 </div>
                                              </div>
                                           </a>
-                                          <a href="pricing-plan.html" class="iq-sub-card setting-dropdown">
-                                             <div class="media align-items-center">
-                                                <div class="right-icon">
-                                                   <i class="ri-settings-4-line text-primary"></i>
-                                                </div>
-                                                <div class="media-body ml-3">
-                                                   <h6 class="mb-0 ">Pricing Plan</h6>
-                                                </div>
-                                             </div>
-                                          </a>
+               
                                           <a href="{{ route('logout') }}" 
                                              class="iq-sub-card setting-dropdown"
                                              onclick="event.preventDefault();document.getElementById('logout-form').submit();">
